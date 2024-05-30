b54f2df5914778fe7dcd8268f83e895023158bdd
Stalias <st.liaskos@gmail.com>
2024-05-30 16:12:04 +0200

    use mock data

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 const path = require('path');
 
+const IS_MOCK = false;
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'ts'],
   async rewrites() {
-    return [
-      { source: '/me/:path*', destination: '/api/mockServer/me/:path*' },
-      { source: '/artist/:path*', destination: '/api/mockServer/artist/:path*' },
-    ];
+    return [createRewritePath('/me/:path*'), createRewritePath('/artist/:path*')];
   },
   env: {
     BE_BASE_URL: process.env.BE_BASE_URL,
@@ -31,3 +30,7 @@ module.exports = {
     return config;
   },
 };
+
+function createRewritePath(path) {
+  return { source: path, destination: `/api${IS_MOCK ? '/mockServer' : ''}${path}` };
+}
--- pages/api/mockServer/artist/search.ts
+++ pages/api/mockServer/artist/search.ts
@@ -1,21 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import headers from '../headers';
-// import artistSearch from '../../../../mocks/responses/artist-search.json'; // TODO show fallback on dev mode (airplane mode..)
+import artistSearch from '../../../../mocks/responses/artist-search.json';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<unknown>) {
-  const requestOptions = {
-    method: 'GET',
-    headers,
-  };
-
-  try {
-    const response = await fetch(
-      `https://release-raccoon.onrender.com/artist/search?pattern=${req.query.pattern}`,
-      requestOptions,
-    );
-    const result = await response.text();
-    res.status(200).json(JSON.parse(result));
-  } catch (e) {
-    console.error('error', e);
-  }
+export default function handler(req: NextApiRequest, res: NextApiResponse<unknown>) {
+  res.status(200).json(artistSearch);
 }
