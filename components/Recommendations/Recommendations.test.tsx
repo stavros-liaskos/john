2e65364f0a758423d71e711c1e8095d75ea875fc
Stalias <st.liaskos@gmail.com>
2024-06-22 01:52:58 +0200

    feat/reco: add some functionality and tests

--- components/Recommendations/Recommendations.test.tsx
+++ components/Recommendations/Recommendations.test.tsx
@@ -1,17 +1,45 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import Recommendations from './Recommendations';
 import { recommendationsI18n } from './Recommendations.data';
+import recommendedArtists from '../../mocks/responses/followed-artists.json';
+import { beforeEachTest } from '../../utils/test-utils';
 
 describe('Recommendations', () => {
+  let originFetch: {
+    (input: RequestInfo | URL, init?: RequestInit | undefined): Promise<Response>;
+  };
+  beforeEach(() => {
+    originFetch = global.fetch;
+
+    beforeEachTest();
+  });
+
+  afterEach(() => {
+    global.fetch = originFetch;
+    jest.restoreAllMocks();
+  });
+
   it('renders without data without crashing', () => {
     // @ts-ignore
     render(<Recommendations />);
   });
 
-  it('renders all elements', () => {
+  it('renders title and artists', async () => {
+    const mRes = { json: jest.fn().mockResolvedValueOnce(recommendedArtists) };
+    const mockedFetch = jest.fn().mockResolvedValueOnce(mRes);
+    console.warn(await mRes.json());
+    global.fetch = mockedFetch;
+
     const { getByText } = render(<Recommendations i18n={recommendationsI18n} />);
 
     expect(getByText(recommendationsI18n.title)).toBeInTheDocument();
+
+    expect(mockedFetch).toHaveBeenCalledTimes(1);
+    await waitFor(() => {
+      expect(mRes.json).toHaveBeenCalledTimes(1);
+    });
+    // const buttons = await findAllByText(recommendationsI18n.artistList.btnTxt); // TODO fix
+    // expect(buttons).toHaveLength(2);
   });
 
   it('matches snapshot', () => {
--- components/Recommendations/Recommendations.tsx
+++ components/Recommendations/Recommendations.tsx
@@ -1,8 +1,23 @@
+import ArtistsList, { ArtistsListI18n } from '../ArtistsList/ArtistsList';
+import React, { useEffect, useState } from 'react';
+import { components } from '../../types/schema';
+
 type RecommendationsI18n = {
   title: string;
+  artistList: ArtistsListI18n;
 };
 
 const Recommendations = ({ i18n }: { i18n: RecommendationsI18n }) => {
+  const [artistLoading, setArtistLoading] = useState<number>(0);
+  const [recommendedArtists, setRecommendedArtists] = useState<components['schemas']['FollowedArtistsResponse']>(
+    [] as components['schemas']['FollowedArtistsResponse'],
+  );
+
+  useEffect(() => {
+    // TODO add Suspense for data fetching
+    fetchRecommendations(setRecommendedArtists);
+  }, []);
+
   if (!i18n || !i18n.title) {
     return null;
   }
@@ -10,9 +25,33 @@ const Recommendations = ({ i18n }: { i18n: RecommendationsI18n }) => {
   return (
     <div>
       <h3 className={'rr-text'}>{i18n.title}</h3>
-      <div>artists lists todo</div>
+      <ArtistsList
+        i18n={i18n.artistList}
+        artistsList={recommendedArtists}
+        onButtonClick={followArtist}
+        artistLoading={artistLoading}
+      />
     </div>
   );
+
+  function followArtist() {
+    setArtistLoading(1);
+    // TODO unite functionality with follow fn from Search
+  }
 };
+
+export function fetchRecommendations(
+  setRecommendedArtists: React.Dispatch<React.SetStateAction<components['schemas']['FollowedArtistsResponse']>>,
+) {
+  fetch(`${process.env.BE_BASE_URL}/artists/recommended?page=0&size=10`, {
+    method: 'GET',
+    credentials: 'include',
+  })
+    .then(res => res.json())
+    .then(result => {
+      return setRecommendedArtists(result.artists); // TODO handle no results
+    })
+    .catch(console.error);
+}
 
 export default Recommendations;
