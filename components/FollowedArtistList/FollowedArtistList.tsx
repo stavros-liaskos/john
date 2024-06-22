695d0075245a7a0f99786965aac628315f8bc1aa
Stalias <st.liaskos@gmail.com>
2024-06-22 14:05:05 +0200

    feat/filter: init filter fn() (#36)

--- components/FollowedArtistList/FollowedArtistList.tsx
+++ components/FollowedArtistList/FollowedArtistList.tsx
@@ -1,35 +1,46 @@
 import React, { useState } from 'react';
-import { ListProps } from './FollowedArtistList.types';
+import { ListI18n, ListProps } from './FollowedArtistList.types';
 import { useArtistsListContext } from '../../contexts/ArtistsList/ArtistsListContext';
 import ArtistsList from '../ArtistsList/ArtistsList';
+import FormInput from '../FormInput/FormInput';
+import { components } from '../../types/schema';
 
 const FollowedArtistList: React.FunctionComponent<ListProps> = ({ i18n }) => {
   const [artistLoading, setArtistLoading] = useState<number>(0);
   const { followedArtistList, setFollowedArtistList } = useArtistsListContext();
 
-  if (
-    !followedArtistList?.rows ||
-    !i18n ||
-    !i18n?.title ||
-    !i18n?.filter ||
-    !i18n?.artistList.noArtists ||
-    !i18n?.artistList.btnTxt ||
-    !i18n?.formInput.label
-  ) {
+  if (validateRequiredData(i18n, followedArtistList)) {
     return null;
   }
 
   return (
     <div>
       <h3 className={'rr-text'}>{i18n.title}</h3>
+      <FormInput handleAction={handleFilter} i18n={i18n.formInput} actionEventTrigger={'onChange'} />
       <ArtistsList
         i18n={i18n.artistList}
-        artistsList={followedArtistList}
+        artistsList={followedArtistList!}
         onButtonClick={unfollowArtist}
         artistLoading={artistLoading}
       />
     </div>
   );
+
+  // TODO handle filter after refactoring followedArtists to be followedArtists.rows and write tests
+  function handleFilter(input: string) {
+    if (!followedArtistList?.rows) {
+      return [];
+    }
+    return followedArtistList.rows.filter(filterArtists);
+
+    function filterArtists(followedArtistList: components['schemas']['FollowedArtistDto']) {
+      if (!input) {
+        return followedArtistList;
+      } else {
+        followedArtistList.name.includes(input);
+      }
+    }
+  }
 
   function unfollowArtist(artistID: number) {
     setArtistLoading(artistID);
@@ -54,5 +65,18 @@ const FollowedArtistList: React.FunctionComponent<ListProps> = ({ i18n }) => {
       .finally(() => setArtistLoading(0));
   }
 };
+
 FollowedArtistList.whyDidYouRender = true;
 export default FollowedArtistList;
+
+function validateRequiredData(i18n: ListI18n, followedArtistList?: components['schemas']['FollowedArtistsResponse']) {
+  return (
+    !followedArtistList?.rows ||
+    !i18n ||
+    !i18n?.title ||
+    !i18n?.filter ||
+    !i18n?.artistList.noArtists ||
+    !i18n?.artistList.btnTxt ||
+    !i18n?.formInput.label
+  );
+}
--- components/FollowedArtistList/FollowedArtistList.types.ts
+++ components/FollowedArtistList/FollowedArtistList.types.ts
@@ -1,12 +1,14 @@
 import { ArtistsListI18n } from '../ArtistsList/ArtistsList';
 
-export type ListProps = {
-  i18n: {
-    title: string;
-    filter: string;
-    artistList: ArtistsListI18n;
-    formInput: {
-      label: string;
-    };
+export type ListI18n = {
+  title: string;
+  filter: string;
+  artistList: ArtistsListI18n;
+  formInput: {
+    label: string;
   };
+};
+
+export type ListProps = {
+  i18n: ListI18n;
 };
