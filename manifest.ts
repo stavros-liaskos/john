03c2a3157f9df29e35c68bf3c3ce5d1032054195
Stalias <st.liaskos@gmail.com>
2024-06-29 21:14:08 +0200

    feat/pwa: add basics in manifest.json

--- manifest.ts
+++ manifest.ts
@@ -6,7 +6,7 @@ export default function manifest(): MetadataRoute.Manifest {
     name: metaI18n.title,
     short_name: metaI18n.shortName,
     description: metaI18n.description,
-    start_url: '/',
+    start_url: metaI18n.startUrl,
     display: 'standalone',
     background_color: '#000000',
     theme_color: '#000000',
@@ -14,6 +14,21 @@ export default function manifest(): MetadataRoute.Manifest {
       {
         src: '/favicon.ico',
         sizes: 'any',
+        type: 'image/x-icon',
+      },
+      {
+        src: '/app_icons/64.png',
+        sizes: '64x64',
+        type: 'image/x-icon',
+      },
+      {
+        src: '/app_icons/192.png',
+        sizes: '192x192',
+        type: 'image/x-icon',
+      },
+      {
+        src: '/app_icons/512.png',
+        sizes: '512x512',
         type: 'image/x-icon',
       },
     ],
--- src/i18n.ts
+++ src/i18n.ts
@@ -2,4 +2,7 @@ export const metaI18n = {
   title: 'Release Raccoon',
   description: 'A music release newsletter',
   shortName: 'RR',
+  startUrl: 'https://www.releaseraccoon.online',
+  display: 'fullscreen',
+  prefer_related_applications: false,
 };
