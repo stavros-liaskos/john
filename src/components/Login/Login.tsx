12fc08b74895eb170192e57744df5aa7796b8b14
Stalias <st.liaskos@gmail.com>
2024-08-04 15:10:14 +0200

    fix/Login: set login ui in layout (#72)

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -8,12 +8,12 @@ const Login: React.FunctionComponent<LoginProps> = ({ i18n }) => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center lg:w-1/2">
-      <h2 className="rr-text text-4xl mb-3">{i18n.welcome}</h2>
-      <p className="rr-text text-xl mb-3">{i18n.text}</p>
+    <div className="flex flex-auto flex-col items-center justify-center w-full">
+      <h2 className="rr-text text-center text-4xl mb-3">{i18n.welcome}</h2>
+      <p className="rr-text text-center text-xl mb-3">{i18n.text}</p>
 
       <Link href="/api/auth/login">
-        <button className="btn btn-large lg:ml-8 my-8 w-44 h-14">{i18n.loginBtn}</button>
+        <button className="btn btn-large lg:ml-8 mt-8 mb-24 w-44 h-14">{i18n.loginBtn}</button>
       </Link>
     </div>
   );
